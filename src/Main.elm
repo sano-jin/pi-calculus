256b7cc97705f107b0f9b0452e193c7d39887889
Jin SANO <sano-jin@akane.waseda.jp>
2020-09-26 11:45:28 +0900

    updated

--- src/Main.elm
+++ src/Main.elm
@@ -6,7 +6,6 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Parser exposing (run, DeadEnd, Problem (..))
 import PiParser as PP
-import Pi
 import Set as S
 import Dict as D
 
@@ -41,7 +40,7 @@ update msg model =
     case msg of
         Eval str -> case run PP.parser str of
                         Ok term -> { model | errors = []
-                                   , result = Pi.show <| Pi.normalize <| Pi.lit2PFN term }
+                                   , result = PP.show term }
                         Err err -> { model | errors = err, result = "error" }
                     
         Change str ->
--- src/PiParser.elm
+++ src/PiParser.elm
@@ -4,12 +4,15 @@ import Parser exposing (..)
 import Char
 import Set
 
-type ProcLit = Receive String String ProcLit -- x?y.P
-             | Send String String ProcLit    -- x!y.P
-             | Parallel ProcLit ProcLit      -- P|Q
-             | Create String ProcLit         -- \x.P
-             | Replicate ProcLit                -- !P
-             | Null                          -- 0
+type Value = Name String
+           | Int Int
+           
+type ProcLit = Send String (List Value)            -- x![v1, ..., vn] 
+             | Receive String (List Value) ProcLit -- x?[v1, ..., vn].P
+             | Create String ProcLit               -- \x.P
+             | Replicate ProcLit                   -- !P
+             | Parallel (List ProcLit)             -- P|Q
+             | Null                                -- 0
 
 flip : (a -> b -> c) -> b -> a -> c
 flip f a b = f b a
@@ -18,28 +21,69 @@ flip f a b = f b a
 lexeme : Parser a -> Parser a
 lexeme p = p |. spaces 
 
-varLit : Parser String
-varLit = variable
-         { start = Char.isAlpha
-         , inner = \c -> Char.isAlphaNum c || c == '_'
-         , reserved = Set.empty
-         }
-          
--- Parser
-paren : Parser a -> Parser a
-paren p =
-    succeed identity
-        |. symbol "("
-        |. spaces
-        |= p
-        |. symbol ")"
-        |. spaces
+nameLit : Parser String
+nameLit =
+    lexeme
+    <| variable
+        { start = Char.isAlpha
+        , inner = \c -> Char.isAlphaNum c || c == '_'
+        , reserved = Set.empty
+        }
+                   
+intLit : Parser Int
+intLit =
+    lexeme
+    <| oneOf
+        [ succeed negate
+        |. lexeme (symbol "-")
+        |= int
+        , int
+        ]
 
+valueLit : Parser Value
+valueLit =
+    oneOf [ Parser.map Name nameLit
+          , Parser.map Int intLit ]
+    
+-- Parser
+sandwitched : String -> String -> Parser a -> Parser a
+sandwitched left right p =
+    succeed identity
+        |. lexeme (symbol left)
+        |= p
+        |. lexeme (symbol right)
+
+paren : Parser a -> Parser a
+paren = sandwitched "(" ")"
+
+sepBy1 : String -> Parser a -> Parser (List a)
+sepBy1 sep p =
+    Parser.map List.singleton p |> andThen (flip loop <| sepByHelp sep p)
+
+sepBy : String -> Parser a -> Parser (List a)
+sepBy sep p =
+    oneOf [ Parser.map List.singleton p |> andThen (flip loop <| sepByHelp sep p)
+          , succeed [] ]
+        
+sepByHelp : String -> Parser a -> List a -> Parser (Step (List a) (List a))
+sepByHelp sep p as_ =
+  oneOf
+    [ succeed (\a -> Loop (a::as_))
+    |. lexeme (symbol sep)
+    |= p
+    , succeed ()
+    |> map (\_ -> Done (List.reverse as_))
+    ]
+
+list : Parser a -> Parser (List a)
+list p =
+    sandwitched "[" "]" <| sepBy "," p 
+      
 receive : String -> Parser ProcLit
 receive channelName =
     succeed (Receive channelName) 
         |. lexeme (symbol "?")
-        |= lexeme varLit
+        |= list valueLit
         |. lexeme (symbol ".")
         |= lazy (\_ -> process)
 
@@ -47,15 +91,13 @@ send : String -> Parser ProcLit
 send channelName =
     succeed (Send channelName) 
         |. lexeme (symbol "!")
-        |= lexeme varLit
-        |. lexeme (symbol ".")
-        |= lazy (\_ -> process)
-
+        |= list valueLit           
+    
 create : Parser ProcLit
 create =
     succeed Create
         |. lexeme (symbol "\\")
-        |= lexeme varLit
+        |= lexeme nameLit
         |. lexeme (symbol ".")
         |= lazy (\_ -> process)
     
@@ -72,7 +114,7 @@ null =
 
 process : Parser ProcLit
 process =
-    oneOf [ (lexeme varLit |> andThen (\n -> oneOf [ receive n, send n ]))
+    oneOf [ (lexeme nameLit |> andThen (\n -> oneOf [ receive n, send n ]))
           , create
           , replicate
           , null
@@ -81,18 +123,8 @@ process =
 
 parallel : Parser ProcLit
 parallel =
-    process |> andThen (flip loop parallelHelp)
-        
-parallelHelp : ProcLit -> Parser (Step ProcLit ProcLit)
-parallelHelp p =
-    oneOf
-    [ succeed (\q -> Loop (Parallel p q))
-        |. lexeme (symbol "|")
-        |= process
-    , succeed ()
-        |> map (\_ -> Done p)
-    ]
-
+    Parser.map Parallel 
+    <| sepBy1 "|" process
                                         
 parser : Parser ProcLit
 parser =
@@ -102,17 +134,28 @@ parser =
        |. end 
 
 -- show
+showValue : Value -> String
+showValue value =
+    case value of
+        Int i -> String.fromInt i
+        Name n -> n
+
 show : ProcLit -> String
 show proc =
+    let showValues vs = "[" ++ String.join "," (List.map showValue vs) ++ "]"
+        showWithParen proc_ = case proc_ of
+                                  Parallel ps -> case ps of
+                                                     [p] -> showWithParen p
+                                                     _ -> "(" ++ show (Parallel ps) ++ ")"
+                                  _ -> show proc_
+    in
     case proc of
-        Receive x y p -> x ++ "?" ++ y ++ "." ++ show p
-        Send x y p -> x ++ "!" ++ y ++ "." ++ show p
-        Parallel p q -> show p ++ "|" ++ show q
-        Create x p -> "new " ++ x ++ "." ++ "(" ++ show p ++ ")"
-        Replicate p -> "!" ++ show p
-        Null -> "0"
-        
-
+        Send x y -> x ++ "!" ++ showValues y
+        Receive x y p -> x ++ "?" ++ showValues y ++ "." ++ showWithParen p
+        Create x p -> "new " ++ x ++ "." ++ showWithParen p
+        Replicate p -> "!" ++ showWithParen p
+        Parallel ps -> String.join "|" <| List.map showWithParen ps
+        Null -> "0"        
 
 problem2String : Problem -> String
 problem2String problem = case problem of
